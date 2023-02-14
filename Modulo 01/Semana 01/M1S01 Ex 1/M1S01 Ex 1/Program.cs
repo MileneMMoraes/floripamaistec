f3d7c4f506b5910a1772189e5453dd107e5185ed
MileneMMoraes <milenemoraes2000@hotmail.com>
2023-02-14 08:53:05 -0300

    Adição semana 01

---
+++ Modulo 01/Semana 01/M1S01 Ex 1/M1S01 Ex 1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Semana 1 -  Exercício 1
+
+int[] numeros = new int[10];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.Write($"Digite o {i + 1}º número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    int contador = 1;
+
+    for (int j = 1; j < numeros[i]; j++)
+    {
+        if (numeros[i] % j == 0)
+        {
+            contador++;
+        }
+    }
+
+    if (contador == 2)
+    {
+        Console.WriteLine($"O número {numeros[i]} é um numero primo e está na posição {i}.");
+    }
+}
---
+++ Modulo 01/Semana 01/M1S01 Ex 4/M1S01 Ex 4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Semana 1 - Exercício 4
+
+string[] nomesAlunos = new string[5];
+decimal[] mediasAlunos = new decimal[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Digite o nome do {i + 1}º aluno: ");
+    nomesAlunos[i] = Console.ReadLine();
+
+    Console.Write($"Digite a média do {i + 1}º aluno: ");
+    mediasAlunos[i] = decimal.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < 5; i++)
+{
+    if (mediasAlunos[i] >= 6)
+    {
+        Console.WriteLine($"O aluno {nomesAlunos[i]} está APROVADO");
+    }
+    else
+    {
+        Console.WriteLine($"O aluno {nomesAlunos[i]} está REPROVADO");
+    }
+}
