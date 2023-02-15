b8ff20af5efad6d79212b529a9fb438891602634
MileneMMoraes <milenemoraes2000@hotmail.com>
2023-02-14 22:12:47 -0300

    Atividades semana 2

---
+++ Modulo 01/Semana 02/M1S02 Ex 3 a 6/M1S02 Ex 3 a 6/Classes/ContaBancaria.cs
@@ -0,0 +1,50 @@
+﻿using M1S02_Ex_3_a_6.Classes.Enum;
+
+namespace M1S02_Ex_3_a_6.Classes
+{
+    public class ContaBancaria
+    {
+        // Semana 02 - Exercício 3
+        public int numero;
+        public int agencia;
+        public string nomeDoTitular;
+        public decimal saldo = 0;
+        public TipoContaEnum tipoConta;
+
+        // Semana 02 - Exercício 4
+        public void Depositar (decimal valor)
+        {
+            if (valor <= 0)
+            {
+                Console.WriteLine("O valor a ser depositado deve ser maior do que 0");
+            }
+            else
+            {
+                saldo = saldo + valor;
+            }
+        }
+
+        // Semana 02 - Exercício 5
+        public void Sacar (decimal valor)
+        {
+            if (valor <= 0) 
+            {
+                Console.WriteLine("O valor a ser sacado deve ser maior do que 0");
+            }
+            else if (valor > saldo)
+            {
+                Console.WriteLine("O valor de saque não pode ser maior que o saldo atual da conta");
+            }
+            else
+            {
+                saldo = saldo - valor;
+            }
+        }
+
+        // Semana 02 - Exercício 6
+        public void ExibirSaldo()
+        {
+            Console.WriteLine($"O saldo atual da conta é de R$ {saldo}");
+        }
+    }
+}
---
+++ Modulo 01/Semana 02/M1S02 Ex 3 a 6/M1S02 Ex 3 a 6/Classes/Enum/TipoContaEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace M1S02_Ex_3_a_6.Classes.Enum
+{
+    // Semana 02 - Exercício 3
+    public enum TipoContaEnum
+    {
+        CORRENTE = 0,
+        POUPANCA = 1
+    }
+}
