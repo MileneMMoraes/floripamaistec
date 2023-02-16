a106bde28ff27d27e01d9050728e88e2dffaa3a9
MileneMMoraes <milenemoraes2000@hotmail.com>
2023-02-15 21:26:18 -0300

    Enviando ex 1 da semana 3 do módulo 1

---
+++ Modulo 01/Semana 03/M1S03 Ex 1/M1S03 Ex 1/Program.cs
@@ -0,0 +1,11 @@
+﻿using M1S03_Ex_1;
+
+Produto produto1 = new Produto("Calculadora", 5.75, 199);
+
+Produto produto2 = new Produto();
+
+Console.WriteLine($"O produto1 tem nome {produto1.nome}, valor: {produto1.preco}" +
+    $", quantidade: {produto1.quantidade}");
+
+Console.WriteLine($"O produto2 tem nome {produto2.nome}, valor: {produto2.preco}" +
+    $", quantidade: {produto2.quantidade}");
